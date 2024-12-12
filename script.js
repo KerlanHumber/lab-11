45fe84038f862cb6b86d2cfde9fc0dcc8ac5080d
Kerlan Augustine <kerlanhumber@gmail.com>
2024-12-12 18:06:23 -0500

    Setted up classes

---
+++ script.js
@@ -0,0 +1,20 @@
+class ProductProperties {
+    constructor(name, price, quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    getTotalValue() {
+        return this.price * this.quantity;
+    }
+
+    toString(){
+        return `Product: ${this.name}, Price: $${this.price.toFixed(2)}, Quantity: ${this.quantity}`;
+    }
+}
+
+// Usage
+const appleProduct = new ProductProperties("Apple", 2.50, 50);
+console.log(appleProduct.toString());
+console.log(appleProduct.getTotalValue());
